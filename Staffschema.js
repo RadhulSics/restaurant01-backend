2de4ae674a2165462eae76e3fb1f8ea47d0ed6c5
Udhith69 <udhithandrose69@gmail.com>
2024-04-18 14:19:22 +0530

    hii

---
+++ Staffschema.js
@@ -0,0 +1,34 @@
+const mongoose=require("mongoose")
+const staffschema=mongoose.Schema({
+    fname:{
+        type:String,
+        required:true
+    },
+    lname:{
+        type:String,
+        required:true
+    },
+    dob:{
+        type:Date,
+        required:true
+    },
+    gender:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    contactno:{
+        type:Number,
+        required:true
+    },
+
+})
+
+module.exports=mongoose.model("newstaffs",staffschema)
