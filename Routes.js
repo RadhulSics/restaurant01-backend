356e4be4827287de13fd2660717f5fd635458d67
PRASHANTH18ps <psprashanth18@gmail.com>
2024-04-30 11:26:01 +0530

    prashanth

--- Routes.js
+++ Routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Customercontroller = require("./Customercontroller");
 const Staffcontroller=require("./Staffcontroller")
+const ordercontrol = require("./ordercontrol")
 module.exports = router;
   
 router.post("/addSchema", Customercontroller.addCustomer);
@@ -10,4 +11,10 @@ router.post("/customerlogin", Customercontroller.customerLogin);
 router.post("/forgotPassword", Customercontroller.forgotPassword);
 router.post('/staffregistration',Staffcontroller.staffregistration);
 router.post('/stafflogin',Staffcontroller.stafflogin);
+router.post('/addorder',ordercontrol.addorder)
+router.get('/vieworder/:userid',ordercontrol.vieworder)
+router.post('/cancelorder',ordercontrol.cancelorder)
+router.post('/paymentstatus',ordercontrol.paymentstatus)
+router.get('/vieworderdetails/:userid',ordercontrol.vieworderdetails)
+router.get('/viewcustomerorder',ordercontrol.viewcustomerorder)
 
