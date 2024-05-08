ef0d1b0f10bcdfec2369d7876f6b9942d5a0eb7d
Praveenps18 <praveenps832002@gmail.com>
2024-05-08 10:03:11 +0530

    commitmsg

--- Routes.js
+++ Routes.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express=require("express")
 const Foodcontroller = require("./Foodcontroller")
 const Customercontroller = require("./Customercontroller");
 const Staffcontroller = require("./Staffcontroller");
-const Foodcontroller = require("./Foodcontroller");
-const ordercontrol = require("./ordercontrol");
 const cartcontrol=require("./Cartcontroller")
-module.exports = router;
+const ordercontrol=require("./ordercontrol")
+const router=express.Router()
+module.exports=router
 
 router.post("/addSchema", Customercontroller.addCustomer);
 
@@ -14,12 +14,19 @@ router.post("/customerlogin", Customercontroller.customerLogin);
 router.post("/forgotPassword", Customercontroller.forgotPassword);
 router.post("/staffregistration", Staffcontroller.staffregistration);
 router.post("/stafflogin", Staffcontroller.stafflogin);
-router.post("/addmenu", Foodcontroller.addmenu);
-router.post("/viewmenu", Foodcontroller.viewmenu);
-router.post("/viewone", Foodcontroller.viewone);
+router.post('/addmenu',Foodcontroller.upload,Foodcontroller.addmenu)
+router.get("/viewmenu", Foodcontroller.viewmenu);
+router.get("/viewone", Foodcontroller.viewone);
 router.post("/editfooddetails/:id", Foodcontroller.editfood);
-router.post("/deletefood", Foodcontroller.deletefood);
+router.post('/deletefood/:id',Foodcontroller.deletefood)
+router.post('/addorder',ordercontrol.addorder)
+router.get('/vieworder/:userid',ordercontrol.vieworder)
+router.post('/cancelorder',ordercontrol.cancelorder)
+router.post('/paymentstatus',ordercontrol.paymentstatus)
+router.get('/vieworderdetails/:userid',ordercontrol.vieworderdetails)
+router.get('/viewcustomerorder',ordercontrol.viewcustomerorder)
 router.post("/addcart/:foodid", cartcontrol.addcart);
 router.get("/viewcart/:userid", cartcontrol.viewcart);
 router.post("/deletecartitem/:id", cartcontrol.deletecartitem);
 router.post("/cartitems/:userid", cartcontrol.deletecart);
+
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(`${__dirname}/upload`));
 const cors = require("cors");
 
-const dbconnection = require("./dbconnection");
+const dbconnection = require("./dbconnection")
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/", router);
